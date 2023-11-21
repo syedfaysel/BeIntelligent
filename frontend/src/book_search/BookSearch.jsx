fb9920c21119e5dba17cf7405e45e2f1419aa62c
Blaiteray <soumitra.phi@gmail.com>
2023-11-21 22:32:53 +0600

    formatting code and finishing book search page

--- frontend/src/book_search/BookSearch.jsx
+++ frontend/src/book_search/BookSearch.jsx
@@ -1,103 +1,296 @@
-import MenuBar from './subcomponents/MenuBar';
-import MiddleSection from './subcomponents/MiddleSection';
-import Footer from './subcomponents/Footer';
-import './BookSearch.css';
-import { useState } from 'react';
+import MenuBar from "./subcomponents/MenuBar";
+import MiddleSection from "./subcomponents/MiddleSection";
+import Footer from "./subcomponents/Footer";
+import "./BookSearch.css";
+import { useState } from "react";
 
-export default function() {
+export default function () {
     const user_name = "Ahsan Habib";
     const recommended_genre = ["romance", "mystery"];
-    const [searched_keyword, change_searched_keyword] = useState('');
-    const [current_genre, change_current_genre] = useState('');
+    const [searched_keyword, change_searched_keyword] = useState("");
+    const [current_genre, change_current_genre] = useState("");
     const [current_page, change_current_page] = useState(1);
-    const genres = ["all" ,"romance", "fiction", "young-adult", "fantasy", "science-fiction", "non-fiction", "children", "history", "mystery", "covers", "horror", "historical-fiction", "best", "gay", "titles", "paranormal", "middle-grade", "love", "contemporary", "historical-romance", "lgbt", "queer", "nonfiction", "thriller", "biography", "women", "series", "lgbtq", "classics", "title-challenge"];
+    const [is_sorted_by_rating, change_is_sorted_by_rating] = useState(false);
+    const genres = [
+        "all",
+        "romance",
+        "fiction",
+        "young-adult",
+        "fantasy",
+        "science-fiction",
+        "non-fiction",
+        "children",
+        "history",
+        "mystery",
+        "covers",
+        "horror",
+        "historical-fiction",
+        "best",
+        "gay",
+        "titles",
+        "paranormal",
+        "middle-grade",
+        "love",
+        "contemporary",
+        "historical-romance",
+        "lgbt",
+        "queer",
+        "nonfiction",
+        "thriller",
+        "biography",
+        "women",
+        "series",
+        "lgbtq",
+        "classics",
+        "title-challenge",
+    ];
     const books = [
         {
-            name : "The Foundation 1 HAHAHAHAHAHHAHAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH",
-            author : "Isaac Asimov",
-            rating : 4.8,
-            genre : ["romance", "fiction"],
+            name: "The Foundation 1 HAHAHAHAHAHHAHAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH",
+            author: "Isaac Asimov",
+            rating: 4.8,
+            genre: ["romance", "fiction"],
         },
         {
-            name : "The Foundation 2",
-            author : "Isaac Asimov hahahhahfshesfsjlfosefesofjesofehsofefhsofeo",
-            rating : 4.8,
-            genre : ["young-adult", "fantasy", "science-fiction"],
+            name: "The Foundation 2",
+            author: "Isaac Asimov hahahhahfshesfsjlfosefesofjesofehsofefhsofeo",
+            rating: 4.8,
+            genre: ["young-adult", "fantasy", "science-fiction"],
         },
         {
-            name : "The Foundation 3",
-            author : "Isaac Asimov",
-            rating : 4.8,
-            genre : ["young-adult", "fantasy", "science-fiction"],
+            name: "The Foundation 3",
+            author: "Isaac Asimov",
+            rating: 4.8,
+            genre: ["young-adult", "fantasy", "science-fiction"],
         },
         {
-            name : "The Foundation 4",
-            author : "Isaac Asimov",
-            rating : 4.8,
-            genre : ["science-fiction", "non-fiction", "children", "history", "mystery", "covers", "horror"],
+            name: "The Foundation 4",
+            author: "Isaac Asimov",
+            rating: 4.0,
+            genre: [
+                "science-fiction",
+                "non-fiction",
+                "children",
+                "history",
+                "mystery",
+                "covers",
+                "horror",
+            ],
         },
         {
-            name : "The Foundation 5",
-            author : "Isaac Asimov",
-            rating : 4.8,
-            genre : ["fantasy", "science-fiction", "non-fiction", "children", "history", "mystery", "covers", "horror", "historical-fiction", "best", "gay", "titles", "paranormal", "middle-grade", "love", "contemporary", "historical-romance", "lgbt", "queer"],
+            name: "The Foundation 5",
+            author: "Isaac Asimov",
+            rating: 4.8,
+            genre: [
+                "fantasy",
+                "science-fiction",
+                "non-fiction",
+                "children",
+                "history",
+                "mystery",
+                "covers",
+                "horror",
+                "historical-fiction",
+                "best",
+                "gay",
+                "titles",
+                "paranormal",
+                "middle-grade",
+                "love",
+                "contemporary",
+                "historical-romance",
+                "lgbt",
+                "queer",
+            ],
         },
         {
-            name : "The Foundation 6",
-            author : "Isaac Asimov",
-            rating : 4.8,
-            genre : ["lgbt", "queer", "nonfiction", "thriller", "biography", "women", "series", "lgbtq", "classics", "title-challenge"],
+            name: "Your Name",
+            author: "Haruku Muraka",
+            rating: 4.8,
+            genre: [
+                "lgbt",
+                "queer",
+                "nonfiction",
+                "thriller",
+                "biography",
+                "women",
+                "series",
+                "lgbtq",
+                "classics",
+                "title-challenge",
+            ],
         },
         {
-            name : "The Foundation 7",
-            author : "Isaac Asimov",
-            rating : 4.8,
-            genre : ["romance", "fiction", "young-adult", "fantasy", "science-fiction"],
+            name: "One Piece",
+            author: "Some Guy",
+            rating: 4.6,
+            genre: [
+                "romance",
+                "fiction",
+                "young-adult",
+                "fantasy",
+                "science-fiction",
+            ],
         },
         {
-            name : "The Foundation 8",
-            author : "Isaac Asimov",
-            rating : 4.8,
-            genre : ["romance", "fiction", "young-adult", "fantasy", "science-fiction", "non-fiction", "children", "history", "mystery", "covers", "horror", "historical-fiction", "best", "gay", "titles", "paranormal", "middle-grade", "love", "contemporary", "historical-romance", "lgbt", "queer", "nonfiction", "thriller", "biography", "women", "series", "lgbtq", "classics", "title-challenge"],
+            name: "Grave of the Fireflies",
+            author: "Japanese Writter",
+            rating: 5,
+            genre: [
+                "romance",
+                "fiction",
+                "young-adult",
+                "fantasy",
+                "science-fiction",
+                "non-fiction",
+                "children",
+                "history",
+                "mystery",
+                "covers",
+                "horror",
+                "historical-fiction",
+                "best",
+                "gay",
+                "titles",
+                "paranormal",
+                "middle-grade",
+                "love",
+                "contemporary",
+                "historical-romance",
+                "lgbt",
+                "queer",
+                "nonfiction",
+                "thriller",
+                "biography",
+                "women",
+                "series",
+                "lgbtq",
+                "classics",
+                "title-challenge",
+            ],
         },
         {
-            name : "The Fool",
-            author : "Mark Toyen",
-            rating : 4.9,
-            genre : ["romance"],
-        }
-    ]
+            name: "The Fool",
+            author: "Mark Toyen",
+            rating: 4.9,
+            genre: ["romance"],
+        },
+    ];
 
-    const recommended_book_list = books.filter((item)=>{
+    books.sort((a, b) => a.name.localeCompare(b.name));
+    const recommended_book_list = books.filter((item) => {
         let choose = false;
-        for(let i=0; i<recommended_genre.length; i++) {
+        for (let i = 0; i < recommended_genre.length; i++) {
             choose = item.genre.includes(recommended_genre[i]) | choose;
         }
         return choose;
     });
 
-    const [books_to_show, change_books_to_show] = useState([...recommended_book_list]);
-    let bookpage_heading_text = searched_keyword ? "Showing Search Result for Keyword \""+searched_keyword+"\"": current_genre ? (current_genre === 'all') ? "Showing All Books" : "Showing Books Related to \""+current_genre+"\"" : "Showing Recommended Books for "+user_name;
-    
-    return (<div className='flex flex-col'>
-    <MenuBar change_searched_keyword={change_searched_keyword} change_current_genre={change_current_genre}/>
-    <div className='flex flex-row bi-bookpage-heading'>
-    <h2 className='text-2xl font-bold dark:text-white'>{bookpage_heading_text}</h2>
-    { (searched_keyword || current_genre) ? <button className="btn btn-sm btn-square btn-outline" onClick={() =>{
-        change_books_to_show([...recommended_book_list]);
-        change_current_page(1);
-        if (current_genre){
-            change_current_genre('');
-        } 
-        if (searched_keyword) {
-            change_searched_keyword('');
-        }
-    }}>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-    </button> : ''} 
-    </div>
-    <MiddleSection genres={genres} books={books} change_current_genre={change_current_genre} change_searched_keyword={change_searched_keyword} books_to_show={books_to_show} change_books_to_show={change_books_to_show} current_page={current_page} change_current_page={change_current_page}/>
-    <Footer />
-    </div>
-    )
+    const [books_to_show, change_books_to_show] = useState([
+        ...recommended_book_list,
+    ]);
+    let bookpage_heading_text = searched_keyword
+        ? 'Showing Search Result for Keyword "' + searched_keyword + '"'
+        : current_genre
+        ? current_genre === "all"
+            ? "Showing All Books"
+            : 'Showing Books Related to "' + current_genre + '"'
+        : "Showing Recommended Books for " + user_name;
+
+    return (
+        <div className="flex flex-col">
+            <MenuBar
+                books={books}
+                change_searched_keyword={change_searched_keyword}
+                change_current_genre={change_current_genre}
+                change_current_page={change_current_genre}
+                change_books_to_show={change_books_to_show}
+                change_is_sorted_by_rating={change_is_sorted_by_rating}
+            />
+            <div className="grid justify-items-stretch grid-cols-2">
+                <div className="flex flex-row bi-bookpage-heading">
+                    <h2 className="text-2xl font-bold dark:text-white">
+                        {bookpage_heading_text}
+                    </h2>
+                    {searched_keyword || current_genre ? (
+                        <button
+                            className="btn btn-sm btn-square btn-outline"
+                            onClick={() => {
+                                change_is_sorted_by_rating(false);
+                                change_books_to_show([
+                                    ...recommended_book_list,
+                                ]);
+                                change_current_page(1);
+                                if (current_genre) {
+                                    change_current_genre("");
+                                }
+                                if (searched_keyword) {
+                                    change_searched_keyword("");
+                                }
+                            }}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        </button>
+                    ) : (
+                        ""
+                    )}
+                </div>
+                <div className="justify-self-end bi-toggle-button">
+                    <button
+                        className="btn btn-sm rounded-none w-48 h-4"
+                        onClick={() => {
+                            // console.log(is_sorted_by_rating);
+                            change_current_page(1);
+                            const books_to_sort = [...books_to_show];
+                            if (is_sorted_by_rating) {
+                                books_to_sort.sort((a, b) =>
+                                    a.name.localeCompare(b.name)
+                                );
+                            } else {
+                                books_to_sort.sort((a, b) => {
+                                    return a.rating == b.rating
+                                        ? 0
+                                        : a.rating > b.rating
+                                        ? -1
+                                        : 1;
+                                });
+                            }
+                            console.log(books_to_sort);
+                            change_books_to_show(books_to_sort);
+                            change_is_sorted_by_rating(!is_sorted_by_rating);
+                        }}
+                    >
+                        {is_sorted_by_rating
+                            ? "Sorted By Rating"
+                            : "Sorted Alphabetically"}
+                    </button>
+                </div>
+            </div>
+            <MiddleSection
+                genres={genres}
+                books={books}
+                change_current_genre={change_current_genre}
+                change_searched_keyword={change_searched_keyword}
+                books_to_show={books_to_show}
+                change_books_to_show={change_books_to_show}
+                current_page={current_page}
+                change_current_page={change_current_page}
+                change_is_sorted_by_rating={change_is_sorted_by_rating}
+            />
+            <Footer />
+        </div>
+    );
 }
--- frontend/src/book_search/subcomponents/MenuBar.jsx
+++ frontend/src/book_search/subcomponents/MenuBar.jsx
@@ -1,8 +1,15 @@
-import { useState } from 'react';
-import './styles/MenuBar.css';
+import { useState } from "react";
+import "./styles/MenuBar.css";
 
-export default function({change_searched_keyword, change_current_genre}) {
-    const [inputval, change_inputval] = useState('');
+export default function ({
+    books,
+    change_searched_keyword,
+    change_current_genre,
+    change_current_page,
+    change_books_to_show,
+    change_is_sorted_by_rating,
+}) {
+    const [inputval, change_inputval] = useState("");
     return (
         <div className="navbar bg-base-100 bi-nav">
             <div className="flex-1">
@@ -10,34 +17,81 @@ export default function({change_searched_keyword, change_current_genre}) {
             </div>
             <div className="flex-none gap-2">
                 <div className="form-control">
-                <input type="text" placeholder="Search Books" className="input input-bordered w-24 md:w-auto bi-searchbox" value={inputval} onKeyDown={(key)=>{
-                    if(key.code === "Enter") {
-                        change_searched_keyword(inputval);
-                        change_current_genre('');
-                        change_inputval('');
-                    }
-                }} onChange={(evnt)=>change_inputval(inputval + evnt.nativeEvent.data)}/>
+                    <input
+                        type="text"
+                        placeholder="Search Books"
+                        className="input input-bordered w-24 md:w-auto bi-searchbox"
+                        value={inputval}
+                        onKeyDown={(key) => {
+                            if (key.code === "Enter" && inputval) {
+                                const filtered_book = books.filter((item) => {
+                                    const exist_in =
+                                        item.name
+                                            .toLowerCase()
+                                            .includes(inputval.toLowerCase()) ||
+                                        item.author
+                                            .toLowerCase()
+                                            .includes(inputval.toLowerCase());
+                                    return exist_in;
+                                });
+                                change_is_sorted_by_rating(false);
+                                change_searched_keyword(inputval);
+                                change_current_page(1);
+                                change_books_to_show(filtered_book);
+                                change_current_genre("");
+                                change_inputval("");
+                            }
+                        }}
+                        onChange={(evnt) => {
+                            if (evnt.nativeEvent.data == null) {
+                                change_inputval(
+                                    inputval.slice(0, inputval.length - 2)
+                                );
+                            } else {
+                                change_inputval(
+                                    inputval + evnt.nativeEvent.data
+                                );
+                            }
+                        }}
+                    />
                 </div>
                 <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                    <div className="w-8 rounded-full">
-                    <img alt="Tailwind CSS Navbar component" src="./src/assets/user_icon.jpg" />
-                    </div>
-                </label>
-                <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                    <li key="profile">
-                    <a className="justify-between">
-                        Profile
-                        <span className="badge">Active</span>
-                    </a>
-                    </li>
-                    <li key="library"><a>Library</a></li>
-                    <li key="book-list"><a>Book List</a></li>
-                    <li key="challenges"><a>Challenges</a></li>
-                    <li key="logout"><a>Logout</a></li>
-                </ul>
+                    <label
+                        tabIndex={0}
+                        className="btn btn-ghost btn-circle avatar"
+                    >
+                        <div className="w-8 rounded-full">
+                            <img
+                                alt="Tailwind CSS Navbar component"
+                                src="./src/assets/user_icon.jpg"
+                            />
+                        </div>
+                    </label>
+                    <ul
+                        tabIndex={0}
+                        className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+                    >
+                        <li key="profile">
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">Active</span>
+                            </a>
+                        </li>
+                        <li key="library">
+                            <a>Library</a>
+                        </li>
+                        <li key="book-list">
+                            <a>Book List</a>
+                        </li>
+                        <li key="challenges">
+                            <a>Challenges</a>
+                        </li>
+                        <li key="logout">
+                            <a>Logout</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
-    )
+    );
 }

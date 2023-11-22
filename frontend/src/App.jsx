162ddff316feacc4eb5c2784a69282606b1c3bb7
unknown <chishty828rahman@gmail.com>
2023-11-22 21:06:36 +0600

    Rasta thik kore disi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
-<<<<<<< HEAD
 import SelectGenre from "./SelectGenre";
 
 function App() {
@@ -17,21 +16,5 @@ function App() {
             </Routes>
         </div>
     );
-=======
-
-function App() {
-  return (
-    <div
-      className="text-white h-[100vh] flex justify-center items-center bg-cover"
-      style={{ backgroundImage: "url('../src/assets/bookstore-bg.jpg')" }}
-    >
-      <Routes>
-        <Route path="login" element={<Login />}></Route>
-        <Route path="register" element={<Register />}></Route>
-      </Routes>
-    </div>
-  );
->>>>>>> 8e9e2eaced6566fe7886a1108c6c846fe807cee3
 }
-
 export default App;

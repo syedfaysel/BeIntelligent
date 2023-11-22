a18f75b657277476230515db487f304fb3a19ece
Blaiteray <soumitra.phi@gmail.com>
2023-11-22 21:43:13 +0600

    merged frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import BookInfo from "./book_info/BookInfo";
 import BookSearch from "./book_search/BookSearch";
+import Login from "./Login";
+import Register from "./Register";
+import SelectGenre from "./SelectGenre";
 export default function () {
     return (
         <Routes>
             <Route path="/booksearch" element={<BookSearch />} />
             <Route path="/bookinfo" element={<BookInfo />} />
+
+            <Route path="login" element={<Login />}></Route>
+            <Route path="register" element={<Register />}></Route>
+            <Route path="selectgenre" element={<SelectGenre />}></Route>
         </Routes>
     );
 }
+        // <div
+        //     className="text-white h-[100vh] flex justify-center items-center bg-cover"
+        //     style={{ backgroundImage: "url('../src/assets/bookstore-bg.jpg')" }}
+        // >
+        // </div>

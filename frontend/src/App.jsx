72466417b7bb51e013384895f26e565d04ee4706
Blaiteray <soumitra.phi@gmail.com>
2023-11-22 20:30:41 +0600

    finished routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,11 @@
+import { Routes, Route } from "react-router-dom";
 import BookInfo from "./book_info/BookInfo";
 import BookSearch from "./book_search/BookSearch";
 export default function () {
-    return <BookSearch />;
+    return (
+        <Routes>
+            <Route path="/booksearch" element={<BookSearch />} />
+            <Route path="/bookinfo" element={<BookInfo />} />
+        </Routes>
+    );
 }
--- frontend/src/book_info/BookInfo.jsx
+++ frontend/src/book_info/BookInfo.jsx
@@ -1,33 +1,46 @@
+import { useLocation } from "react-router-dom";
 import Footer from "../common_components/Footer";
 import MenuDropdown from "../common_components/MenuDropdown";
 import "./BookInfo.css";
 
 export default function () {
-    const image_url = "./src/assets/the_foundation_2.jpg";
-    const book_name = "The Foundation 3";
-    const book_author = "Isaac Asimov";
-    const book_rating = 4.8;
-    const book_genre = [
-        "fantasy",
-        "science-fiction",
-        "non-fiction",
-        "children",
-        "history",
-        "mystery",
-        "covers",
-        "horror",
-        "historical-fiction",
-        "best",
-        "gay",
-        "titles",
-        "paranormal",
-        "middle-grade",
-        "love",
-        "contemporary",
-        "historical-romance",
-        "lgbt",
-        "queer",
-    ];
+    let name, author, rating, genre, image_url;
+
+    try {
+        const { state } = useLocation();
+        image_url = state.image_url;
+        name = state.name;
+        author = state.author;
+        rating = state.rating;
+        genre = state.genre;
+        console.log(state);
+    } catch (e) {
+        image_url = "./src/assets/the_foundation_2.jpg";
+        name = "The Foundation 3";
+        author = "Isaac Asimov";
+        rating = 4.8;
+        genre = [
+            "fantasy",
+            "science-fiction",
+            "non-fiction",
+            "children",
+            "history",
+            "mystery",
+            "covers",
+            "horror",
+            "historical-fiction",
+            "best",
+            "gay",
+            "titles",
+            "paranormal",
+            "middle-grade",
+            "love",
+            "contemporary",
+            "historical-romance",
+            "lgbt",
+            "queer",
+        ];
+    }
 
     return (
         <div className={"flex flex-col bg-[url('" + image_url + "')]"}>
@@ -45,14 +58,14 @@ export default function () {
                         <img
                             src={image_url}
                             alt="Cover"
-                            className="object-cover max-h-full"
+                            className="object-cover max-h-screen"
                         />
                     </div>
                     <div className="flex flex-col justify-self-center self-center gap-2">
                         {/* place inoframtions here */}
                         <div>
-                            <h1 className="mb-4 text-3xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-                                {book_name}
+                            <h1 className="mb-4 text-3xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white break-all">
+                                {name}
                             </h1>
                         </div>
                         <div className="flex flex-row gap-2">
@@ -65,16 +78,16 @@ export default function () {
                                 />
                             </div>
                             <h5 className="text-xl font-bold dark:text-white">
-                                {book_rating}
+                                {rating}
                             </h5>
                         </div>
                         <div className="bi-author-name-display">
-                            <h3 className="text-3xl font-bold dark:text-white">
-                                {book_author}
+                            <h3 className="text-3xl font-bold dark:text-white break-all">
+                                {author}
                             </h3>
                         </div>
                         <div className="flex flex-wrap gap-2">
-                            {book_genre.map((item) => (
+                            {genre.map((item) => (
                                 <span className="bg-gray-100 text-gray-800 text-sm font-medium px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
                                     {item}
                                 </span>

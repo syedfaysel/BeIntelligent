74619c4a7923b0fdd530ce543f84707a08686c1f
Blaiteray <soumitra.phi@gmail.com>
2023-12-02 04:18:47 +0600

    branch merged

--- frontend/src/book_info/BookInfo.jsx
+++ frontend/src/book_info/BookInfo.jsx
@@ -143,7 +143,7 @@ export default function () {
                                 </div>
                                 <div>
                                     <button
-                                        class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+                                        className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
                                         onClick={() => {
                                             navigate_to("/bookreview");
                                         }}
